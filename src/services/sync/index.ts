605d510ba4b85b7719286272187c973a18858dfe
Vishal Vijay <0vishalvijay0@gmail.com>
2020-03-25 21:12:35 +0530

    Remove function sleep

--- src/services/sync/index.ts
+++ src/services/sync/index.ts
@@ -1,19 +1,8 @@
 import functions from 'firebase-functions';
 
-const sleep = (): Promise<void> => new Promise((resolve) => {
-  setTimeout(() => {
-    resolve();
-  }, 2000);
-});
-
 const sync = async (req: functions.Request, res: functions.Response): Promise<void> => {
   // eslint-disable-next-line no-console
   console.log('req.body', req.body);
-
-  await sleep();
-
-  // eslint-disable-next-line no-console
-  console.log('Slept for some time', req.body);
 
   res.status(200).send({
     success: true,
