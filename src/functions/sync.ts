c2d9c24d1d210fff4955d33c71ed1699d0c2aaaf
Vishal Vijay <0vishalvijay0@gmail.com>
2020-03-25 21:05:22 +0530

    Dynamic import

--- src/functions/sync.ts
+++ src/functions/sync.ts
@@ -1,14 +1,8 @@
 import * as functions from 'firebase-functions';
 
 const sync = functions.https.onRequest(async (req, res) => {
-  const importedSync = await import('../services/sync');
-
-  const syncService = importedSync as unknown as (
-    req: functions.Request,
-    res: functions.Response,
-  ) => void;
-
-  syncService(req, res);
+  const syncService = (await import('../services/sync')).default;
+  await syncService(req, res);
 });
 
 export default sync;
