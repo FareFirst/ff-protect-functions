6966430770c5051c2b8074a6e8bbfbbdb9969b3b
Vishal Vijay <0vishalvijay0@gmail.com>
2020-03-25 22:28:07 +0530

    Add intial fucction after test

--- src/functions/constants.ts
+++ src/functions/constants.ts
@@ -1,5 +1,4 @@
-const DEFAULT_GCP_REGION = 'asia-east2';
-
+const DEFAULT_GCP_REGION = 'asia-east2'; // Cloud function not available in Mumbai region
 
 export {
   // eslint-disable-next-line import/prefer-default-export
