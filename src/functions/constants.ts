285743610f9643d7cfc5b767729cdde3d5916373
Vishal Vijay <0vishalvijay0@gmail.com>
2020-03-25 21:21:01 +0530

    Setup asia region

---
+++ src/functions/constants.ts
@@ -0,0 +1,7 @@
+const DEFAULT_GCP_REGION = 'asia-east2';
+
+
+export {
+  // eslint-disable-next-line import/prefer-default-export
+  DEFAULT_GCP_REGION,
+};
--- src/functions/sync.ts
+++ src/functions/sync.ts
@@ -1,8 +1,15 @@
 import * as functions from 'firebase-functions';
+import { DEFAULT_GCP_REGION } from './constants';
 
-const sync = functions.https.onRequest(async (req, res) => {
-  const syncService = (await import('../services/sync')).default;
-  await syncService(req, res);
-});
+const runtimeOpts: functions.RuntimeOptions = {
+  memory: '128MB',
+};
+
+const sync = functions.runWith(runtimeOpts)
+  .region(DEFAULT_GCP_REGION)
+  .https.onRequest(async (req, res) => {
+    const syncService = (await import('../services/sync')).default;
+    await syncService(req, res);
+  });
 
 export default sync;
